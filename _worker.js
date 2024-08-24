5167d47775921228430019d482cd004a6c03d7c5
Beichen92 <129265343+Beichen92@users.noreply.github.com>
2024-08-25 01:15:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,15 +10,15 @@ let userID = '5641bb1a-7bb7-4b7f-a47c-91ab256662e6';
 
 // https://ipdb.030101.xyz/bestproxy/
 // https://www.nslookup.io/domains/bestproxy.030101.xyz/dns-records/
-const proxyIPs= ['cdnfd.920324.xyz', 'bpb.yousef.isegaro.com', 'bestproxy.030101.xyz'];
-// '8.219.209.14', '104.19.46.161'
+const proxyIPs= ['cdnfd.920324.xyz'];
+// '8.219.209.14', '104.19.46.161', 'bpb.yousef.isegaro.com', 'bestproxy.030101.xyz'
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
-let dohURL = 'https://cloudflare-dns.com/dns-query';
+let dohURL = 'https://dns.google/dns-query';
 
 let panelVersion = '2.5';
 
